97f728867bc15ec664295dc8d2372d0ac2b0d550
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-08-25 17:52:40 -0500

    Added routes pt1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/about', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../public/about.html'));
+});
+
+router.get('/listing', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../public/listing.html'));
+});
+
+router.get('/contact', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../public/contact.html'));
 });
 
 module.exports = router;
