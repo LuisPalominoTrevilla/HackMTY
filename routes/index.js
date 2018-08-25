0030e4c529a28d7bf716b14d524024d71958c46c
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-08-25 17:59:44 -0500

    Added new route

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,8 @@ router.get('/contact', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../public/contact.html'));
 });
 
+router.get('/negocio/:name', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../public/negocio.html'));
+});
+
 module.exports = router;
