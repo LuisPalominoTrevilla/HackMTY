24ee0c7ee02cfc24bbffa50b69276d927def2b8a
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-08-25 19:48:17 -0500

    API POST

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../db');
+const mysql = require('mysql');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   pool.getConnection((err, con) => {
     if (err) throw err;
     con.query("SELECT * FROM shop", (err, result) => {
@@ -11,7 +12,20 @@ router.get('/', function(req, res, next) {
       if (err) throw err;
       res.json(result);
     });
-  })
+  });
 });
+
+router.post('/login', (req, res, next) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  pool.getConnection((err, con) => {
+    if (err) throw err;
+    con.query("SELECT client_id FROM client WHERE mail = " + mysql.escape(username) + " AND password = "+ mysql.escape(password), (err, result) => {
+      con.release();
+      if (err) throw err;
+      res.send("HOlA MUNDO");
+    });
+  });
+})
 
 module.exports = router;
